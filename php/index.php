fb8de6841cf4e90682576f9a1b2e492ef7eacf97
Vlktna <v.volokitinaa@gmail.com>
2020-09-08 19:46:33 +0300

    new SVG

--- php/index.php
+++ php/index.php
@@ -3,6 +3,7 @@
 session_start();
 $_SESSION['result'] = array();
 ?>
+
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +11,7 @@ $_SESSION['result'] = array();
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
-<body>
+<body onload="drawPlat()">
 <div class="header">
     <span>Вариант 2607</span>
     <span class="stud_name">Волокитина Вероника, P3230</span>
@@ -18,12 +19,53 @@ $_SESSION['result'] = array();
 
 <div class="container">
 
-    <div class="image">
-        <object type="image/svg+xml" data="../img/plot.svg">
-            <img src="../img/plot.svg" alt="plot">
-        </object>
-    </div>
+    <svg height="600" width="600">
 
+        <polygon class="rectangle-graph" fill=#95a3b3
+                 points="300,300 300,200 100,200 100,300"></polygon>
+
+        <polygon class="rectangle-graph" fill=#95a3b3
+                 points="300,300 300,200 400,300"></polygon>
+
+        <polygon class="rectangle-graph" fill=#95a3b3
+                 points="300,300 401,300 300,401"></polygon>
+
+        <path class="circle-graph" d="M 400,300 A 100,100 0 0, 1 300,400"  fill=#95a3b3></path>
+
+        <line class="axis" stroke="black" x1="50" x2="550" y1="300" y2="300"></line>
+        <line class="axis" stroke="black" x1="300" x2="300" y1="50" y2="550"></line>
+
+        <line class="arrow" stroke="black" x1="535" x2="550" y1="295" y2="300"></line>
+        <line class="arrow" stroke="black" x1="535" x2="550" y1="305" y2="300"></line>
+
+        <line class="arrow" stroke="black" x1="295" x2="300" y1="65" y2="50"></line>
+        <line class="arrow" stroke="black" x1="300" x2="305" y1="50" y2="65"></line>
+
+        <line class="point-line" stroke="black" x1="400" x2="400" y1="295" y2="305"></line>
+        <line class="point-line" stroke="black" x1="500" x2="500" y1="295" y2="305"></line>
+        <line class="point-line" stroke="black" x1="200" x2="200" y1="295" y2="305"></line>
+        <line class="point-line" stroke="black" x1="100" x2="100" y1="295" y2="305"></line>
+
+        <line class="point-line" stroke="black" x1="295" x2="305" y1="400" y2="400"></line>
+        <line class="point-line" stroke="black" x1="295" x2="305" y1="500" y2="500"></line>
+        <line class="point-line" stroke="black" x1="295" x2="305" y1="200" y2="200"></line>
+        <line class="point-line" stroke="black" x1="295" x2="305" y1="100" y2="100"></line>
+
+        <text class="point-text" x="310" y="205" font-size="20px">R/2</text>
+        <text class="point-text" x="310" y="105" font-size="20px">R</text>
+        <text class="point-text" x="310" y="405" font-size="20px">-R/2</text>
+        <text class="point-text" x="310" y="505" font-size="20px">-R</text>
+
+        <text class="point-text" x="95" y="290" font-size="20px">-R</text>
+        <text class="point-text" x="195" y="290" font-size="20px">-R/2</text>
+        <text class="point-text" x="395" y="290" font-size="20px">R/2</text>
+        <text class="point-text" x="495" y="290" font-size="20px">R</text>
+
+        <text class="point-text" x="550" y="290" font-size="20px">Y</text>
+        <text class="point-text" x="310" y="60" font-size="20px">X</text>
+
+        <circle cx="150" cy="150" fill="red" id="target-dot" r="0"></circle>
+    </svg>
 
     <form class="getValue" id="form" action="check.php" target="result_table">
 
@@ -81,22 +123,16 @@ $_SESSION['result'] = array();
 
         <div class="form_block">
             <button name="check" type="submit">CHECK</button>
-            <button name="clear" type="submit" onclick="clear.php">CLEAR</button>
-
         </div>
 
     </form>
 
 </div>
 
-
-<center>
-    <div>
-        <iframe name="result_table" id="result_table">
-        </iframe>
-    </div>
-</center>
-
+<div class="container-table">
+    <iframe name="result_table" id="result_table">
+    </iframe>
+</div>
 
 
 </body>
